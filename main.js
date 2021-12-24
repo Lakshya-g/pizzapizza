60fa5d2fe7edacdff99421cff89e64719b554461
Lakshya-g <92384531+Lakshya-g@users.noreply.github.com>
2021-12-24 20:42:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+menu_list_array = ["Veg Margherita Pizza",
+                        "5 Cheese Pizza",
+                        "4 Cheese Pizza",
+                        "Only Veg! Pizza",
+                        "Only Non-Veg! Pizza",
+                        "Deluxe All Toppings Pizza (For Non veg and veg people)"];
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="Pizzeria.jpg">'
+        var item=document.getElementById("add_item").value;
+         menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
